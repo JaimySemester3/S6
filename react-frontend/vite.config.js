bc466fc372be6fd947a90e0fe2b41d89df4a431d
JaimyDerks <315190@student.fontys.nl>
2025-06-18 14:37:17 +0200

    updated front-end endpoints

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -7,12 +7,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/user': 'http://microservices.local',  
-      '/tweets': 'http://microservices.local', 
-      '/timeline': 'http://microservices.local', 
+      //'/user': 'http://microservices.local',  
+      //'/tweets': 'http://microservices.local', 
+      //'/timeline': 'http://microservices.local', 
 
       // '/tweets': 'http://20.13.192.26:3000',    
       // '/timeline': 'http://4.207.239.104:4000' 
+
+      '/user': 'http://20.13.207.138',
+      '/tweets': 'http://20.13.207.138',
+      '/timeline': 'http://20.13.207.138'
     }
   }
 })
